8eedd0106e932db301ace1f8c44ec374875db484
Duncan Gough <duncan@suttree.com>
2024-04-17 14:30:42 +0100

    Update pmlvsn.sh
    one fails, another works

--- pmlvsn.sh
+++ pmlvsn.sh
@@ -1,2 +1,3 @@
 #ffmpeg -framerate 1/0.5 -pattern_type glob -i "px/img/orig/*.jpg" -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
-ffmpeg -framerate 1/0.5 -pattern_type -pattern_type glob -i "px/mov/*.mov" -filter_complex "[0:v][1:v]concat=n=2:v=1:a=0,format=yuv420p[v]" -map "[v]" -c:v libx264 -r 30 -movflags +faststart output.mp4
+#ffmpeg -framerate 1/0.5 -pattern_type -pattern_type glob -i "px/mov/*.mov" -filter_complex "[0:v][1:v]concat=n=2:v=1:a=0,format=yuv420p[v]" -map "[v]" -c:v libx264 -r 30 -movflags +faststart output.mp4
+ffmpeg -f concat -safe 0 -i <(for f in *.MOV; do echo "file '$PWD/$f'"; done) -c copy output.mov
